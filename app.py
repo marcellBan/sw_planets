1504b5768db0ade8d7c31db0dda6ace3a5f91cb9
Bán Marcell <afga93+cc@gmail.com>
2017-06-19 16:40:14 +0200

    added residents modal
    still unhappy with html structure and style
    need to add loading indicator

--- app.py
+++ app.py
@@ -8,14 +8,26 @@ app = Flask(__name__)
 def format_planet_data(planet_data):
     for planet in planet_data.get('results'):
         if planet.get('diameter') != 'unknown':
-            planet['diameter'] = '{:,}'.format(int(planet['diameter']))
+            planet['diameter'] = '{:,} km'.format(int(planet['diameter']))
         if planet.get('population') != 'unknown':
             planet['population'] = '{:,}'.format(int(planet['population']))
 
 
+def construct_modal_data(planet, residents):
+    data = dict()
+    data['modal_title'] = '{}\'{} residents'.format(planet['name'], '' if planet['name'].endswith('s') else 's')
+    data['residents'] = residents
+    for resident in residents:
+        if resident.get('height') != 'unknown':
+            resident['height'] = '{} m'.format(int(resident['height']) / 100)
+        if resident.get('mass') != 'unknown':
+            resident['mass'] = '{} kg'.format(resident['mass'])
+    return data
+
+
 @app.route('/')
 def index():
-    planet_data = requests.get('https://swapi.co/api/planets').json()
+    planet_data = requests.get('http://swapi.co/api/planets').json()
     format_planet_data(planet_data)
     parent_template = app.jinja_env.get_template('index.html')
     return render_template('planets_table.html', parent_template=parent_template, data=planet_data)
@@ -27,6 +39,15 @@ def get_table():
     format_planet_data(planet_data)
     return render_template('planets_table.html', data=planet_data)
 
+
+@app.route('/get-modal-content')
+def get_modal_content():
+    planet = requests.get(request.args.get('url')).json()
+    residents = list()
+    for resident in planet['residents']:
+        residents.append(requests.get(resident).json())
+    data = construct_modal_data(planet, residents)
+    return render_template('residents_modal.html', data=data)
 
 if __name__ == '__main__':
     app.run()
--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,32 @@
 $(document).ready(function() {
     $('#prev-page').click(pageButtonClick);
     $('#next-page').click(pageButtonClick);
+    $('.residents-modal-activator').click(loadModalData);
+    $('#residents-modal').on('shown.bs.modal', function(){
+        $('#residents-modal .modal-body').height($('#residents-modal table').height());
+        $(this).css('opacity', '1');
+        $('#residents-modal .modal-dialog').css('transform', '');
+    });
 });
 
-function pageButtonClick(){
+function loadModalData(event) {
+    var url = $(this).data('purl');
+    $('#residents-modal .modal-content').load(`/get-modal-content?url=${url}`, function(){
+        $('#residents-modal').modal('show');
+        $('#residents-modal').css('opacity', '0');
+        $('#residents-modal .modal-dialog').css('transform', 'translate(0,-25%)');
+    });
+}
+
+function pageButtonClick() {
     var dataUrl = $(this).data('url');
     getTable(dataUrl);
 }
 
 function getTable(url) {
-    $.ajax({
-        url: `/get-table?url=${url}`,
-        success: function(result) {
-            $('#table-wrapper').html(result);
-            $('#prev-page').click(pageButtonClick);
-            $('#next-page').click(pageButtonClick);
-        }
+    $('#table-wrapper').load(`/get-table?url=${url}`, function(){
+        $('#prev-page').click(pageButtonClick);
+        $('#next-page').click(pageButtonClick);
+        $('.residents-modal-activator').click(loadModalData);
     });
 }
